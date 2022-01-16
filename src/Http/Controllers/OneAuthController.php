3474bca0cb308b9fc311a2d4e5d3fddc9a9dbde1
Avazbek Nematov <68123420+avacoderuz@users.noreply.github.com>
2022-01-16 17:46:14 +0500

    a

--- src/Http/Controllers/OneAuthController.php
+++ src/Http/Controllers/OneAuthController.php
@@ -12,7 +12,7 @@ class OneAuthController extends Controller
 
     public function auth(){
         $url = env("ONE_ID_API_URL")."?response_type=one_code&client_id=".env('ONE_ID_CLIENT_ID','')
-            .'&redirect_uri="'.env('ONE_ID_REDIRECT_URI').'"&scope='.env('ONE_ID_SCOPE', '')."&state=testState";
+            .'&redirect_uri='.env('ONE_ID_REDIRECT_URI').'&scope='.env('ONE_ID_SCOPE', '')."&state=testState";
 
         return redirect($url);
     }
